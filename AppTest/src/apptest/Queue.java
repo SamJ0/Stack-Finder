27a2e4e4f69e55a656b004578f5b60adb2ae5b2e
SamJ0 <98806203+SamJ0@users.noreply.github.com>
2023-05-29 23:32:49 +0300

    Update Queue.java

--- AppTest/src/apptest/Queue.java
+++ AppTest/src/apptest/Queue.java
@@ -1,10 +1,11 @@
-
 package apptest;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Queue {
 
-    private int[] data;
+    private List<Integer> data;
     private int capacity;
 
     private int front = 0;
@@ -12,72 +13,59 @@ public class Queue {
     private int size = 0;
 
     Queue(int capacity) {
-
         this.capacity = capacity;
-        data = new int[capacity];
-
+        data = new ArrayList<>(capacity);
     }
 
     public int size() {
-
         return size;
-
     }
 
     public boolean isEmpty() {
-
         return (size == 0);
-
     }
 
     public void enQueue(int element) {
-
         if (size == capacity) {
             System.out.println("Queue Overflow");
             return;
         }
 
         rear++;
-        data[rear] = element;
+        data.add(rear, element);
         size++;
-
     }
 
     public int first() {
-
         if (isEmpty()) {
             return 0;
         }
 
-        return data[front];
+        return data.get(front);
     }
 
     public int deQueue() {
-
         if (isEmpty()) {
             return 0;
         }
 
-        int answer = data[front];
+        int answer = data.get(front);
         front++;
         size--;
 
         return answer;
-
     }
 
     public void display() {
-
         if (size == 0) {
             System.out.println("Queue is Empty");
             return;
         }
 
-        for (int i = front; i < rear; i++) {
-            System.out.print(data[i] + " ");
+        for (int i = front; i <= rear; i++) {
+            System.out.print(data.get(i) + " ");
         }
 
         System.out.println("\n");
-
     }
 }
